f93cb0b5c7af03ed8a9167e9e93deb8758c73957
unkabas <undercannabis10@gmail.com>
2025-06-26 13:43:09 +0200

    setJWT added

--- controllers/login.go
+++ controllers/login.go
@@ -9,34 +9,36 @@ import (
 
 func Login(c *gin.Context) {
 	var input models.InputAuth
+
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(400, gin.H{
-			"message": "Not a JSON",
-		})
+		c.JSON(400, gin.H{"message": "Not a JSON"})
 		return
 	}
 	if input.Username == "" || input.Password == "" {
-		c.JSON(400, gin.H{
-			"message": "No username or password added",
-		})
+		c.JSON(400, gin.H{"message": "No username or password added"})
 		return
 	}
+
 	var userFound models.User
-	if err := config.DB.Where("username=? ", input.Username).First(&userFound).Error; err != nil {
-		c.JSON(401, gin.H{
-			"message": "Incorrect username, try to sign in",
-		})
-	} else {
-		match := services.CheckPasswordHash(input.Password, userFound.Password)
-		if !match {
-			c.JSON(401, gin.H{
-				"message": "Incorrect username or password",
-			})
-		} else {
-			c.JSON(200, gin.H{
-				"message": "success",
-				"token":   "soon will be jwt",
-			})
-		}
+	if err := config.DB.Where("username=?", input.Username).First(&userFound).Error; err != nil {
+		c.JSON(401, gin.H{"message": "Incorrect username, try to sign in"})
+		return
 	}
+
+	match := services.CheckPasswordHash(input.Password, userFound.Password)
+	if !match {
+		c.JSON(401, gin.H{"message": "Incorrect username or password"})
+		return
+	}
+
+	tokenString, err := services.SetJWT(userFound.Username)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Cant generate Token"})
+		return
+	}
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(200, gin.H{
+		"message": "success login",
+	})
 }
---
+++ services/setJWT.go
@@ -0,0 +1,21 @@
+package services
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"time"
+)
+
+func SetJWT(username string) (string, error) {
+	secret := os.Getenv("SECRET")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": username,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
